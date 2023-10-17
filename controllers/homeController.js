c53068b52230f35af4b6868f7368b41ee8e6cfb0
Genzoo <genzoo.kontakt@gmail.com>
2023-10-17 21:14:08 +0200

    Navbar improvements, handling navbar subsites

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -11,3 +11,31 @@ exports.getAddPage = (req, res) =>{
     };
     res.render('add', viewsData);
 };
+
+exports.getCategoryPage = (req, res) =>{
+    const viewsData = {
+        pageTitle: 'GitPad - Kategorie',
+    };
+    res.render('categories', viewsData);
+};
+
+exports.getLibraryPage = (req, res) =>{
+    const viewsData = {
+        pageTitle: 'GitPad - Biblioteka',
+    };
+    res.render('library', viewsData);
+};
+
+exports.getAboutPage = (req, res) =>{
+    const viewsData = {
+        pageTitle: 'GitPad - About us',
+    };
+    res.render('about', viewsData);
+};
+
+exports.getAccountPage = (req, res) =>{
+    const viewsData = {
+        pageTitle: 'GitPad - Konto',
+    };
+    res.render('account', viewsData);
+};
